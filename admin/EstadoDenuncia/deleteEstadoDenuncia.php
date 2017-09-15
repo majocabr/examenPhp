a55507b643ab1e97b874f80a238b34760e958247
majocabr <majocabr.93@gmail.com>
2017-09-15 12:50:52 -0500

    collector

--- admin/EstadoDenuncia/deleteEstadoDenuncia.php
+++ admin/EstadoDenuncia/deleteEstadoDenuncia.php
@@ -6,7 +6,12 @@ session_start();
 <html lang="es">
 	<head>
 		<meta charset ="utf-8">
-		<title> Eliminar EstadoDenuncia</title>
+<title>Eliminar Estado de Denuncia</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="../../css/tablas.css" rel="stylesheet" >
 	</head>
 <body>
 
@@ -17,15 +22,32 @@ session_start();
 </section>
 <aside>
 <?php
-
+echo "<nav class='navbar navbar-default'>";
+	  echo "<div class='container-fluid'>";
+	    echo "<div class='navbar-header'><a class='navbar-brand' >Tabla Usuario</a></div>";
+		echo " <ul class='nav navbar-nav'>";
+		      	echo "<li><a href='../readsupremo.php'>Menú</a></li>";
+			echo "<li><a href='nuevoestadodenuncia.php'>Nuevo</a></li>";
+			echo "<li><a href='readEstadoDenuncia.php'>Consulta</a></li>";
+		echo "</ul>";
+		echo " <ul class='nav navbar-nav navbar-right'>";
+			echo "<li><a href='#'>Hola Usuario : (" . $_SESSION ['MiSession'] . ")</a></li>";
+			echo "<li><a href='../salir.php'><span class='glyphicon glyphicon-log-out'></span>Salir</a></li>";
+		echo "</ul>";
+	  echo "</div>";
+	echo "</nav>";
 $id=$_GET["id"];
-
 include_once("EstadoDenunciaCollector.php");
 $EstadoDenunciaCollectorObj= new EstadoDenunciaCollector();
-$EstadoDenunciaCollectorObj->deleteEstadoDenunciante($id);
-echo "valor id: ".htmlspecialchars($id)." ha sido eliminado correctamente </br>";
+$EstadoDenunciaCollectorObj->deleteEstadoDenuncia($id);
+echo "<br>";
+echo "<div class='container'>";
+echo "<div class='panel panel-default'>";
+echo "<div class='panel-heading'>Estado Denuncia Eliminado Correctamente</div>";
+echo "<div class='panel-body'>$no</div>";
+echo "</div>";
+echo "</div>";
 ?>
-
 <div class="text-fieldsl">
          <a href='readEstadoDenuncia.php'>Volver al inicio</a>                                    
 </div>
